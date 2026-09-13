ba7c1637fb0dfb456a555320ac49ff2e8da75710
John Doe <froemmel.hendrik@gmail.com>
2026-09-13 22:00:19 +0200

    ...

--- packages/react/src/presentation/texts.ts
+++ packages/react/src/presentation/texts.ts
@@ -92,7 +92,7 @@ export const standardTexte = {
   'kiosk.keepPlaying': 'Weiterspielen',
   'kiosk.playAgain': 'Nochmal spielen',
   'kiosk.buzzer': 'Buzzern',
-  'kiosk.submit': 'Antwort abgeben und auflösen',
+  'kiosk.submit': 'Antwort abgeben',
   'kiosk.secondChance': '{player}, du darfst es jetzt auch versuchen',
   'kiosk.continue': 'Weiter',
 } as const
